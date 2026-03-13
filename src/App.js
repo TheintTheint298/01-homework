70703d70a084f31133a16e264304538e72927a75
TheintTheint298 <theinttheintsoehtet@gmail.com>
2026-03-13 09:33:44 +0630

    active image

--- src/App.js
+++ src/App.js
@@ -13,25 +13,6 @@ function App() {
   const [activeImage, setActiveImage] = useState(images[0]);
 
   return (
-    // <div className="container">
-    //   <div>
-    //     <img className="main-image" src={activeImage} alt="Active" />
-    //   </div>
-
-    //   <div className="img-container">
-    //     {images.map((img, index) => (
-    //       <img
-    //         key={index}
-    //         src={img}
-    //         alt={`Thumbnail ${img}`}
-    //         onClick={() => setActiveImage(img)}
-    //         style={{
-    //           border: activeImage === img ? "3px solid blue" : "1px solid gray",
-    //         }}
-    //       />
-    //     ))}
-    //   </div>
-    // </div>
     <div className="container">
       <div>
         <img className="main-image" src={activeImage} alt="Active" />
